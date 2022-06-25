dd2ee3938752d67479b981726f50a28bc93e64e2
Joe Yates <joe.g.yates@gmail.com>
2022-06-25 18:10:44 +0200

    Remove debugging

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-console.log('Loading svizzerina index.js...')
 require('../css/svizzerina.sass')
 
 const initialize = () => {
   console.log('initialize svizzerina')
 }
-console.log('initialize function:', initialize)
-
-const foo = () => console.log('foooooooooooo!')
 
 export default initialize
-export {foo}
